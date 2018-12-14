8217976f8c9aa466617144189ef1cd19026e2aee
Sam007961 <24758349+sam007961@users.noreply.github.com>
2018-12-14 07:05:04 +0200

    implemented local step.

--- ClothApp/MassSpringSolver.cpp
+++ ClothApp/MassSpringSolver.cpp
@@ -1,95 +1,94 @@
 #include "MassSpringSolver.h"
 
-StateBuffer::StateBuffer() : _prev_state(new State()), _prev_prev_state(new State()) {}
-StateBuffer::StateBuffer(State* const _prev_state, State* const _prev_prev_state)
-	: _prev_state(new State(_prev_state)), _prev_prev_state(new State(_prev_prev_state)) {}
-
-StateBuffer::State* StateBuffer::prev_state() const { return _prev_state; }
-StateBuffer::State* StateBuffer::prev_prev_state() const { return _prev_prev_state; }
-
-void StateBuffer::push(State* state) {
-	delete _prev_prev_state;
-	_prev_prev_state = _prev_state;
-	_prev_state = state;
-}
-
-StateBuffer::~StateBuffer() { delete _prev_state; delete _prev_prev_state; }
-
 mass_spring_system::mass_spring_system(
 	unsigned int n_points,
 	unsigned int n_springs,
 	float time_step,
+	EdgeList spring_list,
 	VectorXf rest_lengths,
-	float damping_factor,
-	SparseMatrix system_matrix,
-	SparseMatrix M,
-	SparseMatrix J,
-	VectorXf fext
-) 
+	VectorXf stiffnesses,
+	VectorXf M,
+	VectorXf fext,
+	float damping_factor
+)
 	: n_points(n_points), n_springs(n_springs),
 	time_step(time_step), rest_lengths(rest_lengths),
-	damping_factor(damping_factor), M(M), L(L), J(J),
-	fext(fext) {}
+	damping_factor(damping_factor), M(M.asDiagonal()),
+	fext(fext), spring_list(spring_list) {
+	// compute L and J
+
+}
 
 MassSpringSolver::MassSpringSolver(mass_spring_system* system, float* vbuff) 
-	: system(system), current_state(vbuff, system->n_points * 3) {
-	// short hands
-	float h2 = system->time_step * system->time_step;
-	float a = system->damping_factor;
+	: system(system), current_state(vbuff, system->n_points * 3), 
+	prev_state(current_state), spring_directions(system->n_springs * 3) {
+	
+	float h2 = system->time_step * system->time_step; // shorthand
+	float a = system->damping_factor; // shorthand
 	
 	// pre-factor system matrix
 	SparseMatrix A = system->M + h2 * system->L;
 	system_matrix.analyzePattern(A);
 	system_matrix.factorize(A); 
-
-	// compute local step to find initial spring directions
-	localStep();
-
-	// compute initial inertial term
-	computeInertialTerm();
-	
-}
-
-void MassSpringSolver::computeInertialTerm() {
-	// short hands
-	float a = system->damping_factor;
-
-	// compute inertial term
-	inertial_term = (a + 1) * (*prev_states.prev_state())
-		- a * (*prev_states.prev_prev_state());
 }
 
 void MassSpringSolver::globalStep() {
-	// short hands
-	float h2 = system->time_step * system->time_step;
-	float a = system->damping_factor;
-	VectorXf& y = inertial_term;
+	float h2 = system->time_step * system->time_step; // shorthand
 
 	// compute right hand side
-	VectorXf b = system->M * y + h2 * system->J * spring_directions
+	VectorXf b = system->M * inertial_term
+		+ h2 * system->J * spring_directions
 		+ h2 * system->fext;
+
+	// save current state in previous state
+	prev_state = current_state;
 
 	// solve system and update state
 	current_state = system_matrix.solve(b);
 }
 
 void MassSpringSolver::localStep() {
+	unsigned int j = 0;
+	for (auto i : system->spring_list) {
+		Vector3f p12(
+			current_state[3 * i.first + 0] - current_state[3 * i.second + 0],
+			current_state[3 * i.first + 1] - current_state[3 * i.second + 1],
+			current_state[3 * i.first + 2] - current_state[3 * i.second + 2]
+		);
 
-}
-
-void MassSpringSolver::solveStep() {
-	globalStep();
-	localStep();
-}
-
-void MassSpringSolver::solveSteps(unsigned int n) {
-	for (unsigned int i = 0; i < n; i++) {
-		solveStep();
+		p12.normalize();
+		spring_directions[3 * j + 0] = 	system->rest_lengths[j] * p12[0];
+		spring_directions[3 * j + 1] =	system->rest_lengths[j] * p12[1];
+		spring_directions[3 * j + 2] =	system->rest_lengths[j] * p12[2];
+		j++;
 	}
 }
 
-void MassSpringSolver::pushState() {
-	VectorXf* state = new VectorXf(current_state);
-	prev_states.push(state);
-	computeInertialTerm();
+void MassSpringSolver::solve(unsigned int n) {
+	float a = system->damping_factor; // shorthand
+
+	// update inertial term (current_state is next prev_state)
+	inertial_term = (a + 1) * (current_state)-a * prev_state;
+	for (unsigned int i = 0; i < n; i++) {
+		localStep();
+		globalStep();
+	}
+}
+
+void MassSpringSolver::timedSolve(unsigned int ms) {
+	// TODO
+}
+
+
+mass_spring_system* MassSpringBuilder::UniformGrid(
+	unsigned int n,
+	float time_step,
+	float rest_length,
+	float stiffness,
+	float mass,
+	float damping_factor,
+	float gravity,
+	float* points
+) {
+
 }
--- ClothApp/MassSpringSolver.h
+++ ClothApp/MassSpringSolver.h
@@ -1,58 +1,44 @@
 #pragma once
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
-
-class StateBuffer {
-public:
-	typedef Eigen::VectorXf State;
-private:
-	State *_prev_state, *_prev_prev_state;
-public:
-	StateBuffer();
-	StateBuffer(State* const _prev_state,
-		State* const _prev_prev_state);
-	State* prev_state() const;
-	State* prev_prev_state() const;
-	void push(State* const state);
-	~StateBuffer();
-
-};
+#include <vector>
 
 struct mass_spring_system { 
 	typedef Eigen::SparseMatrix<float> SparseMatrix;
 	typedef Eigen::VectorXf VectorXf;
+	typedef std::vector<std::pair<unsigned int, unsigned int> > EdgeList;
 	
 	// parameters
-	unsigned int n_points;
-	unsigned int n_springs;
-	float time_step;
-	VectorXf rest_lengths;
-	float damping_factor;
+	unsigned int n_points; // number of points
+	unsigned int n_springs; // number of springs
+	float time_step; // time step
+	EdgeList spring_list; // spring edge list
+	VectorXf rest_lengths; // spring rest lengths
+	VectorXf fext; // external forces
+	float damping_factor; // damping factor
 	
 
 	// system matrices
 	SparseMatrix M;
 	SparseMatrix L;
 	SparseMatrix J;
-
-	// external forces
-	VectorXf fext;
 	
 	mass_spring_system(
 		unsigned int n_points,       // number of points
 		unsigned int n_springs,      // number of springs
 		float time_step,             // time step
+		EdgeList spring_list,        // spring edge list
 		VectorXf rest_lengths,       // spring rest lengths
-		float damping_factor,        // damping factor
-		SparseMatrix M,              // mass matrix
-		SparseMatrix L,              // L matrix
-		SparseMatrix J,              // J matrix
-		VectorXf fext                // external forces
+		VectorXf stiffnesses,        // spring stiffnesses
+		VectorXf M,                  // node masses
+		VectorXf fext,               // external forces
+		float damping_factor         // damping factor
 	);
 };
 
 class MassSpringSolver {
 private:
+	typedef Eigen::Vector3f Vector3f;
 	typedef Eigen::VectorXf VectorXf;
 	typedef Eigen::SparseMatrix<float> SparseMatrix;
 	typedef Eigen::SimplicialCholesky<Eigen::SparseMatrix<float> > Cholesky;
@@ -64,12 +50,9 @@ private:
 
 	// state
 	Map current_state;
-	StateBuffer prev_states;
+	VectorXf prev_state;
 	VectorXf spring_directions;
 	VectorXf inertial_term;
-
-	// inertial term computation
-	void computeInertialTerm();
 
 	// steps
 	void globalStep();
@@ -79,21 +62,21 @@ public:
 	MassSpringSolver(mass_spring_system* system, float* vbuff);
 
 	// solve iterations
-	void solveStep();
-	void solveSteps(unsigned int n);
-
-	// move to next time step
-	void pushState();
-
+	void solve(unsigned int n);
+	void timedSolve(unsigned int ms);
 };
 
-static mass_spring_system* UniformGrid(
-	unsigned int n,          // grid size
-	float time_step,         // time step
-	float rest_length,       // spring rest length (non-diagonal)
-	float stiffness,         // spring stiffness
-	float mass,              // node mass
-	float damping_factor,    // damping factor
-	float gravity,           // gravitationl force (-z axis)
-	float* points            // vertex buffer
-);
+
+class MassSpringBuilder {
+public:
+	static mass_spring_system* UniformGrid(
+		unsigned int n,          // grid size
+		float time_step,         // time step
+		float rest_length,       // spring rest length (non-diagonal)
+		float stiffness,         // spring stiffness
+		float mass,              // node mass
+		float damping_factor,    // damping factor
+		float gravity,           // gravitationl force (-z axis)
+		float* points            // vertex buffer
+	);
+};
